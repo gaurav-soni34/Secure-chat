6214c2d0f9d39260040b7674a304737ef36cca33
gaurav-soni34 <gauravsoni1967@gmail.com>
2026-02-25 20:58:11 +0530

    ..

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,11 +1,11 @@
-import { treaty } from "@elysiajs/eden"
-import type { App } from "../app/api/[[...slugs]]/route"
-
-export const client = treaty<App>("/").api
-
 // import { treaty } from "@elysiajs/eden"
 // import type { App } from "../app/api/[[...slugs]]/route"
 
-// export const client = treaty<App>(
-//   process.env.NEXT_PUBLIC_API_URL!
-// ).api
+// export const client = treaty<App>("/").api
+
+import { treaty } from "@elysiajs/eden"
+import type { App } from "../app/api/[[...slugs]]/route"
+
+export const client = treaty<App>(
+  process.env.NEXT_PUBLIC_API_URL!
+).api
